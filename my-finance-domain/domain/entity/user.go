665947e5c1fac89e2e9efea1b4c3ac0a01873cc6
Rafael Waterkemper <rafaelwaterkemper@Rafaels-MacBook-Air.local>
2024-08-03 00:36:50 -0300

    Starting

---
+++ my-finance-domain/domain/entity/user.go
@@ -0,0 +1,49 @@
+package domain
+
+type User struct {
+	id             int
+	identification string
+	email          string
+	cpf            string
+}
+
+func (u User) GetId() int {
+	return u.id
+}
+
+type UserBuilder struct {
+	user User
+}
+
+func (b UserBuilder) New() *UserBuilder {
+	return &UserBuilder{}
+}
+
+func (b *UserBuilder) Id(id int) *UserBuilder {
+	b.user.id = id
+	return b
+}
+
+func (b *UserBuilder) Identification(identification string) *UserBuilder {
+	b.user.identification = identification
+	return b
+}
+
+func (b *UserBuilder) Email(email string) *UserBuilder {
+	b.user.email = email
+	return b
+}
+
+func (b *UserBuilder) Cpf(cpf string) *UserBuilder {
+	b.user.cpf = cpf
+	return b
+}
+
+func (b *UserBuilder) Wallet(wallet *Wallet) *UserBuilder {
+	b.user.wallet = wallet
+	return b
+}
+
+func (b *UserBuilder) Build() User {
+	return b.user
+}
